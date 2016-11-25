fe18954dbcad575bbe5532cff48b8f5eccffa09c
RexLiu <rexliu@thinkgeo.com>
2016-11-25 18:31:50 +0800

    Upload code.

---
+++ HighlightAtMouseHover/TestWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+using ThinkGeo.MapSuite.Wpf;
+
+namespace HighlightAtMouseHover
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        private PointShape mousePosition = null;
+        private DispatcherTimer highlightTimer = null; 
+
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sets the correct map unit and the extent of the map.
+            wpfMap1.MapUnit = GeographyUnit.DecimalDegree;
+            wpfMap1.CurrentExtent = new RectangleShape(-128,51,-65,19);
+            wpfMap1.Background = new SolidColorBrush(Color.FromRgb(148, 196, 243));
+
+            WorldMapKitWmsWpfOverlay worldMapKitWmsWpfOverlay = new WorldMapKitWmsWpfOverlay();
+            wpfMap1.Overlays.Add(worldMapKitWmsWpfOverlay);
+
+            LayerOverlay layerOverlay = new LayerOverlay();
+            ShapeFileFeatureLayer shapeFileLayer = new ShapeFileFeatureLayer(@"../../data/USStates.shp");
+            shapeFileLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.CreateSimpleAreaStyle(GeoColor.FromArgb(20, GeoColor.StandardColors.LightGreen),
+                                                                                                        GeoColor.StandardColors.LightGray);
+            shapeFileLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            layerOverlay.Layers.Add("StatesLayer", shapeFileLayer);
+            wpfMap1.Overlays.Add("LayerFeatureOverlay", layerOverlay);
+
+            //Highlight layer
+            InMemoryFeatureLayer highlightLayer = new InMemoryFeatureLayer();
+            highlightLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.CreateSimpleAreaStyle(GeoColor.FromArgb(120, GeoColor.StandardColors.LightGreen),
+                                                                                                        GeoColor.StandardColors.LightGray);
+            highlightLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            LayerOverlay highlightOverlay = new LayerOverlay();
+            highlightOverlay.Layers.Add("HighlightLayer", highlightLayer);
+            wpfMap1.Overlays.Add("HighlightOverlay", highlightOverlay);
+
+          
+            wpfMap1.Refresh();
+        }
+
+       
+        private void wpfMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Logic for Highlighting
+            mousePosition = wpfMap1.ToWorldCoordinate(e.GetPosition(wpfMap1));
+
+            if (highlightTimer != null)
+            {
+                highlightTimer.Stop();
+            }
+
+            highlightTimer = new DispatcherTimer();
+            highlightTimer.Interval = new TimeSpan(1000000);
+            highlightTimer.Tick += new System.EventHandler(timer_Tick);
+            highlightTimer.Start();   
+
+
+            //Gets the PointShape in world coordinates from screen coordinates.
+            Point point = e.MouseDevice.GetPosition(null);
+            
+            ScreenPointF screenPointF = new ScreenPointF((float)point.X, (float)point.Y);
+            PointShape pointShape = ExtentHelper.ToWorldCoordinate(wpfMap1.CurrentExtent, screenPointF, (float)wpfMap1.Width, (float)wpfMap1.Height);
+
+            textBox1.Text = "X: " + DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(pointShape.X) + 
+                          "  Y: " + DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(pointShape.Y);
+
+           }
+
+        void timer_Tick(object sender, System.EventArgs e)
+        {
+            HighlightAFeatureContainingPoint(mousePosition);
+            highlightTimer.Stop();
+        }
+
+        private void HighlightAFeatureContainingPoint(PointShape point)
+        {
+            LayerOverlay highlightOverlay = (LayerOverlay)wpfMap1.Overlays["HighlightOverlay"];
+            ShapeFileFeatureLayer featureLayer = (ShapeFileFeatureLayer)((LayerOverlay)wpfMap1.Overlays["LayerFeatureOverlay"]).Layers["StatesLayer"];
+            InMemoryFeatureLayer highlightLayer = (InMemoryFeatureLayer)highlightOverlay.Layers["HighlightLayer"];
+
+            featureLayer.Open();
+            Collection<Feature> features = featureLayer.QueryTools.GetFeaturesContaining(point, ReturningColumnsType.NoColumns);
+            featureLayer.Close();
+
+            if (features.Count != 0)
+            {
+                highlightLayer.Open();
+                highlightLayer.InternalFeatures.Clear();
+                highlightLayer.InternalFeatures.Add(features[0]);
+                highlightLayer.Close();
+            }
+            else
+            {
+                highlightLayer.Open();
+                highlightLayer.InternalFeatures.Clear();
+                highlightLayer.Close();
+            }
+            highlightOverlay.Refresh();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# HighlightAtMouseHoverSample-ForWpf
+
+### Description
+
+                In this WPF project, we show you how to highlight a feature when the user hovers the mouse pointer over it. This effect can be realized in Map Suite by using a timer and doing some spatial querying based on the location of the mouse pointer. This sample works with area-based features but the same could be accomplished with point- or line-based features simply by using different spatial queries.
+              
+
+![Screenshot](https://github.com/ThinkGeo/HighlightAtMouseHoverSample-ForWpf/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0.0](http:mapsuite.nuget)
+
+### About the Code
+
+Working...
+
+### Getting Help
+
+[Map Suite Wpf Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_wpf_desktop_edition)
+
+[Map Suite Wpf Product Description](http://thinkgeo.com/map-suite-developer-gis/wpf-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
